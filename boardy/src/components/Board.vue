7df62e7b215e5e0b56b17f5dda389c6bf21ef86e
root <root@DESKTOP-VFO2AFT.localdomain>
2020-09-07 15:42:45 +0200

    Added vue.js project

---
+++ boardy/src/components/Board.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="board">
+    <vs-button>Hello World</vs-button>
+  </div>
+</template>
+
+<script>
+ import Vuesax from 'vuesax'
+
+export default {
+  name: 'Board',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
